b82fe10539ac36a16df86a7debef427e9d2331a8
adhi1123 <106170157+adhi1123@users.noreply.github.com>
2025-08-02 20:39:24 +0530

    Use .env for secret key and DB URI

--- website/__init__.py
+++ website/__init__.py
@@ -2,14 +2,18 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from os import path
 from flask_login import LoginManager
+from dotenv import load_dotenv  
+import os                       
+
+load_dotenv()  
 
 db = SQLAlchemy()
 DB_NAME = "database.db"
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'qdfbnkjhgddhjk'
-    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')  
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')  
     db.init_app(app)
 
     from .views import views
